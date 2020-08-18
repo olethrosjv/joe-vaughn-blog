e0d3121c355e8af26acee43c5b04e0c842d3910c
olethrosjv <chefjoevaughn@gmail.com>
2020-08-18 12:53:18 -0700

    trying still

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
      // resolve: `gatsby-plugin-google-analytics`,
       //options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
+      //},
     },
     `gatsby-plugin-feed`,
     {
