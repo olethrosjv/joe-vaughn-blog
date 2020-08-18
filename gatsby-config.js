dfa4e65dfef3f860281aa0901f1a9d24467566b2
olethrosjv <chefjoevaughn@gmail.com>
2020-08-18 12:48:12 -0700

    trying to fix for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,8 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
+     // resolve: `gatsby-plugin-google-analytics`,
+      //options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
@@ -78,6 +78,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
